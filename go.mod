6bec47f45248cce42e65d2d4ac0493c7b0602c50
Mijgona <mijgon.9191@gmail.com>
2021-04-01 20:53:15 +0500

    Добавленны статусы платежей

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/mijgona/bank
+module github.com/mijgona/bank/v2
 
 go 1.16
--- pkg/types/types.go
+++ pkg/types/types.go
@@ -4,6 +4,15 @@ type Money int64
 
 //Category представляет собой категорию, в которой был совершён платеж (авто, аптеки, рестораны и т.д.)
 type Category string
+//Status представляет собой статус платежа
+type Status string
+
+//Предопределённые статусы платежей
+const(
+	StatusOk Status="OK"
+	StatusFail Status="FAIL"
+	StatusInProgress Status="INPROGRESS"
+)
 
 //Payment представляет информацию о платеже
 type Payment struct {
